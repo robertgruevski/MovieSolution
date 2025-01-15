4abba992bb464768549e90ca772e9b19f2b733da
robertgruevski <robertgruevski@outlook.com>
2025-01-15 12:26:52 -0500

    Movie context, seed data, EF connection string

---
+++ MovieProject/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MovieProject.Models
+{
+	public class MovieContext : DbContext
+	{
+		public DbSet<Movie> Movies { get; set; } = null!; // a Property that represents the collection of all Movie objects
+		public MovieContext(DbContextOptions<MovieContext> options) : base(options) { } // Movie constructor
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Movie>().HasData(
+				new Movie() { MovieId = 1, Name = "The Godfather", Year = 1972, Rating = 5 },
+				new Movie() { MovieId = 2, Name = "Casablanca", Year = 1942, Rating = 4 },
+				new Movie() { MovieId = 3, Name = "The Matrix", Year = 1999, Rating = 4 }
+				);
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
--- MovieProject/Program.cs
+++ MovieProject/Program.cs
@@ -1,14 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using MovieProject.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add EF Core Dependency Injection
+builder.Services.AddDbContext<MovieContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+	app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
@@ -17,7 +24,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
