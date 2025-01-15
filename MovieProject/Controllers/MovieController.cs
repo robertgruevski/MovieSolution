a1b5004fc7dcf04efb41ab3a065d200e91b45159
robertgruevski <robertgruevski@outlook.com>
2025-01-15 13:06:22 -0500

    Add/Edit movie functionality

--- MovieProject/Controllers/MovieController.cs
+++ MovieProject/Controllers/MovieController.cs
@@ -17,6 +17,30 @@ namespace MovieProject.Controllers
 			ViewBag.Action = "Add New Movie";
 			return View("Edit", new Movie());
 		}
+		[HttpPost]
+		public IActionResult Edit(Movie movie)
+		{
+			if(ModelState.IsValid)
+			{
+				// Either Add a new movie or Edit a movie
+				if(movie.MovieId == 0)
+				{
+					Context.Movies.Add(movie);
+				}
+				Context.SaveChanges();
+				return RedirectToAction("Index", "Home");
+				//else
+				//{
+				//	Context.Movies.Update(movie);
+				//}
+			}
+			else
+			{
+				// Show Validation Errors
+				ViewBag.Action = (movie.MovieId == 0) ? "Add" : "Edit";
+				return View(movie);
+			}
+		}
 		public IActionResult Index()
 		{
 			return View();
