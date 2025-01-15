9ceae70ef9dec31c35080b2ad33f569f006ef46f
robertgruevski <robertgruevski@outlook.com>
2025-01-15 12:07:50 -0500

    Movie model

---
+++ MovieProject/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MovieProject.Models
+{
+	public class Movie
+	{
+		public int MovieId { get; set; }
+		[Required(ErrorMessage = "Please enter a name")]
+		public string Name { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Please enter a year")]
+		[Range(1900,2024,ErrorMessage = "Year must be between 1900 and 2025")]
+		public int? Year { get; set; }
+		[Required(ErrorMessage = "Please enter a rating")]
+		[Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+		public int? Rating { get; set; }
+	}
+}
